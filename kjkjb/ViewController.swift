0359b52002f6e2e7b7755e43452376719370abc6
Armen Safarian <arm5267@yandex.ru>
2022-05-18 20:42:19 +0400

    Initial Commit

---
+++ kjkjb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kjkjb
+//
+//  Created by Armen Safarian on 18.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ kjkjbUITests/kjkjbUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  kjkjbUITestsLaunchTests.swift
+//  kjkjbUITests
+//
+//  Created by Armen Safarian on 18.05.22.
+//
+
+import XCTest
+
+class kjkjbUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
